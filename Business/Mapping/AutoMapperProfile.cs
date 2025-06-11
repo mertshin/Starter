54e138a9b3a2ebcf339f7bcb8cdc67fc9aa9bfd6
mertt <mert.shnm@gmail.com>
2025-06-11 14:12:38 +0300

    Add project files.

---
+++ Business/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;     // DTO'ların namespace'i
+using Core.Entities;    // Entitylerin namespace'i
+
+namespace Business.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // Örnek mapping: Entity -> DTO ve DTO -> Entity
+            //CreateMap<Product, ProductDto>().ReverseMap();
+            //CreateMap<Category, CategoryDto>().ReverseMap();
+
+            // Buraya diğer entity <-> DTO maplemelerini ekleyebilirsin
+        }
+    }
+}
